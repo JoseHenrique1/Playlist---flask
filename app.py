71bb3724d17d165c82c7fb7a54d4987a587d0e65
JoseHenrique1 <104796730+JoseHenrique1@users.noreply.github.com>
2023-01-28 00:53:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ db = SQLAlchemy(app)
 from views import *
 from models import *
 
+with app.app_context():
+        db.create_all()
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
